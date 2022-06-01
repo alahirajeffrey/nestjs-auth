80698a8d836815cf3460e8a835f1af7a29238248
alahirajeffrey <alahirajeffrey@gmail.com>
2022-05-31 23:14:36 -0700

    setup postgre and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities:true,
+      synchronize:true
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
