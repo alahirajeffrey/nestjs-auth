20618e232f18394bb7ce7210a1cbccfbc42eab56
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-01 01:42:46 -0700

    edited user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,12 +1,13 @@
-import {PrimaryGeneratedColumn, Column, BeforeInsert, CreateDateColumn } from 'typeorm'
+import {PrimaryGeneratedColumn, Column, BeforeInsert, CreateDateColumn, Entity } from 'typeorm'
 import * as bcrypt from 'bcrypt'
 
+@Entity('user')
 export class UserEntity{
     @PrimaryGeneratedColumn('uuid')
     id : string
 
     @Column()
-    fullName : string
+    email : string
     
     @Column()
     password: string
