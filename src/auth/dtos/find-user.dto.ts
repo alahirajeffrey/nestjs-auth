e1bbd8b08a4d0fca8f8b408799bd943faad63c38
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-01 01:43:21 -0700

    added DTO fields

--- src/auth/dtos/find-user.dto.ts
+++ src/auth/dtos/find-user.dto.ts
@@ -1,6 +1,13 @@
-import { IsEmail } from "class-validator";
+import { IsEmail, IsString } from "class-validator";
 
 export class FindUserDto{
+
+    @IsString()
+    id: string
+    
     @IsEmail()
     email : string
+
+    @IsString()
+    password : string
 }
