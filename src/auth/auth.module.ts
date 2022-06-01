1dea852aebd61db8c5a1fc2fd19877556996bf17
alahirajeffrey <alahirajeffrey@gmail.com>
2022-06-01 07:46:59 -0700

    fixed imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-    imports:[],
-    providers:[],
-    controllers:[]
+    imports:[TypeOrmModule.forFeature([UserEntity]),
+    PassportModule.register({
+        defaultStrategy: 'jwt',
+        property: 'user',
+        session: false
+    }),
+    JwtModule.register({
+        secret: `${process.env.SECRETKEY}`, 
+        signOptions: {expiresIn: 3600},
+    })],
+    providers:[AuthService, JwtStrategy],
+    controllers:[AuthController]
 })
 export class AuthModule {}
